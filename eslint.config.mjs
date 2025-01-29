9813d8f43feecd93e656929077f7a37883fdef84
kanakanho <kahoshibakichi@gmail.com>
2025-01-29 19:19:46 +0900

    init

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  react: true,
+  typescript: true,
+}, {
+  rules: {
+    'ts/consistent-type-definitions': ['error', 'type'],
+  },
+})
